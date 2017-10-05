c013076fbc6dd3234d58f0b96d9cb5dcaa016f60
Tommie Nygren <tommie.nygren@gmail.com>
2017-10-05 14:09:11 +0200

    Created project with libgdx project thingy

---
+++ src/android/src/com/lavaeater/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.lavaeater;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.lavaeater.WastelandKids;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new WastelandKids(), config);
+	}
+}
---
+++ src/desktop/src/com/lavaeater/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lavaeater.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lavaeater.WastelandKids;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WastelandKids(), config);
+	}
+}
