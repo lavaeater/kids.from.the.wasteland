e0f7f8650db16dcab5541c0999e06599bb03a47e
Tommie Nygren <tommie.nygren@gmail.com>
2017-11-23 10:31:26 +0100

    Yurp

--- src/core/src/com/lavaeater/mapstuff/TIleMap.kt
+++ src/core/src/com/lavaeater/mapstuff/TIleMap.kt
@@ -5,7 +5,10 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
 import ktx.math.vec2
 
-open class Entity(var pos:Vector2, val texture: Texture, val width:Float, val height:Float) {
+open class Entity(var pos:Vector2, val texture: Texture) {
+    var width:Float = 0f
+    var height:Float = 0f
+
     fun draw(batch: SpriteBatch) {
         batch.draw(texture, pos.x, pos.y, width, height)
     }
@@ -17,5 +20,9 @@ enum class TileType {
     Cliff
 }
 
-class Tile(val x:Float, val y:Float, val size: Int, val type: TileType, texture: Texture): Entity(vec2(x,y), texture, width, height)
+class Tile(val x:Float, val y:Float, val size: Int, val type: TileType, texture: Texture): Entity(vec2(x,y), texture) {
+    val isGrass = type == TileType.Grass
+    val isWater = type == TileType.Water
+    val isCliff = type == TileType.Cliff
+}
 
