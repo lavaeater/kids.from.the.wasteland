15b4813f49b997ade7480a5b90d91a3822159657
Tommie Nygren <tommie.nygren@vironova.com>
2018-02-12 16:05:40 +0100

    Recreated a brand new, shiny, project from scratch

---
+++ kftw/src/desktop/src/com/lavaeater/kftw/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lavaeater.kftw.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lavaeater.kftw.KidsFromTheWasteLandGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KidsFromTheWasteLandGame(), config);
+	}
+}
