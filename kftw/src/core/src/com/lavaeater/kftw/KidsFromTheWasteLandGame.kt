edd2ae3d690606c694ba2a00c68e0030da0fd6c2
Tommie Nygren <tommie.nygren@gmail.com>
2018-02-15 22:53:24 +0100

    WTF, why is it tracking workspace?

--- kftw/src/core/src/com/lavaeater/kftw/KidsFromTheWasteLandGame.kt
+++ kftw/src/core/src/com/lavaeater/kftw/KidsFromTheWasteLandGame.kt
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.lavaeater.Assets
 
 class KidsFromTheWasteLandGame : ApplicationAdapter() {
     private lateinit var batch : SpriteBatch
@@ -17,7 +18,9 @@ class KidsFromTheWasteLandGame : ApplicationAdapter() {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         batch.begin()
-
+        for(spriteCol in Assets.sprites.values)
+            for(sprite in spriteCol.values)
+                sprite.draw(batch)
         batch.end()
     }
 
