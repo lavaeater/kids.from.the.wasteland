39798edf2a25d9d48c891ee76c485130a1ad9bc2
Tommie Nygren <tommie.nygren@vironova.com>
2018-02-16 16:51:19 +0100

    I re-added the support for Ashley since, well, I don't care about performance - yet

---
+++ kftw/src/core/src/com/lavaeater/kftw/managers/WorldManager.kt
@@ -0,0 +1,10 @@
+package com.lavaeater.kftw.managers
+
+import com.badlogic.ashley.core.Component
+import com.badlogic.ashley.core.PooledEngine
+import ktx.ashley.*
+
+class WorldManager {
+
+    fun CreateEntity() : Entity
+}
