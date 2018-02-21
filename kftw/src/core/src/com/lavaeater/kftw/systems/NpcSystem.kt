3347169e0ad8897e51da1ba8122e05cf14678ab1
Tommie Nygren <tommie.nygren@gmail.com>
2018-02-21 08:15:34 +0100

    WIP, AI for steering or decision making

--- kftw/src/core/src/com/lavaeater/kftw/systems/NpcSystem.kt
+++ kftw/src/core/src/com/lavaeater/kftw/systems/NpcSystem.kt
@@ -1,11 +1,22 @@
 package com.lavaeater.kftw.systems
 
+import com.badlogic.gdx.ai
 import com.badlogic.ashley.core.Entity
-import com.badlogic.ashley.core.EntitySystem
 import com.badlogic.ashley.systems.IteratingSystem
+import com.badlogic.gdx.ai.steer.Steerable
+import com.badlogic.gdx.math.Vector2
 import com.lavaeater.kftw.components.NpcComponent
 import ktx.ashley.allOf
 import ktx.ashley.mapperFor
+
+
+
+class SteeringSystem : IteratingSystem(allOf(NpcComponent::class).get()), Steerable<Vector2> {
+    override fun processEntity(entity: Entity?, deltaTime: Float) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
 
 class NpcSystem : IteratingSystem(allOf(NpcComponent::class).get()) {
     val mapper = mapperFor<NpcComponent>()
@@ -13,5 +24,12 @@ class NpcSystem : IteratingSystem(allOf(NpcComponent::class).get()) {
         var npcComponent = mapper[entity]
 
     }
+}
 
+
+
+class AiDef {
+    val def = "root" +
+            "   selector" +
+            "       "
 }
