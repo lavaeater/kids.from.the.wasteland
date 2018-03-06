79a73786765d2b6fda21c0ea7b728cb89f76d589
Tommie Nygren <tommie.nygren@vironova.com>
2018-03-06 19:54:34 +0100

    WIP - HUD, updated gradle stuff etc

--- kftw/src/core/src/com/lavaeater/kftw/ui/Hud.kt
+++ kftw/src/core/src/com/lavaeater/kftw/ui/Hud.kt
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.utils.Disposable
 import com.badlogic.gdx.utils.viewport.FitViewport
@@ -15,7 +14,7 @@ import ktx.vis.table
 
 
 
-class Hud() : Disposable {
+class Hud : Disposable {
 
   var stage: Stage
   private val viewport: Viewport
@@ -25,12 +24,9 @@ class Hud() : Disposable {
     Scene2DSkin.defaultSkin =  Skin(Gdx.files.internal("skins/uiskin.json"))
     //setup the HUD viewport using a new camera seperate from gamecam
     //define stage using that viewport and games spritebatch
-
-
-
-
     viewport = FitViewport(Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat(), OrthographicCamera())
     stage = Stage(viewport, batch)
+    setup()
   }
 
   fun update(delta: Float) {
@@ -48,12 +44,13 @@ class Hud() : Disposable {
 
   fun setup() {
     stage.clear()
-//    val table = table {
-//    }
-//    table.setFillParent(true)
-//
-//    //add table to the stage
-//    stage.addActor(table)
+    val table = table {
+      label("Just a teststring")
+    }
+    table.setFillParent(true)
+
+    //add table to the stage
+    stage.addActor(table)
   }
 
   fun showInventory() {
