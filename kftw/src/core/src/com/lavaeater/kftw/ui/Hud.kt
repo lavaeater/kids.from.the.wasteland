3a091ae75d41704aa6d3f8dd4bfbd6a4e4d0b344
Tommie Nygren <tommie.nygren@vironova.com>
2018-03-07 13:58:22 +0100

    WIP - HUD

--- kftw/src/core/src/com/lavaeater/kftw/ui/Hud.kt
+++ kftw/src/core/src/com/lavaeater/kftw/ui/Hud.kt
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Cell
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.utils.Disposable
 import com.badlogic.gdx.utils.viewport.FitViewport
@@ -11,6 +12,7 @@ import com.badlogic.gdx.utils.viewport.Viewport
 import com.kotcrab.vis.ui.VisUI
 import com.kotcrab.vis.ui.util.adapter.AbstractListAdapter
 import com.kotcrab.vis.ui.util.adapter.SimpleListAdapter
+import com.kotcrab.vis.ui.widget.ListView
 import com.lavaeater.kftw.data.Player
 import com.lavaeater.kftw.injection.Ctx
 import ktx.vis.*
@@ -23,7 +25,7 @@ class Hud : Disposable {
   val batch = Ctx.context.inject<SpriteBatch>()
   val player = Ctx.context.inject<Player>()
   var inventoryListAdapter : SimpleListAdapter<String>
-  lateinit var inventoryTable: Table
+  lateinit var inventory: Cell<*>
 
   init {
     VisUI.load("tixel/tixel.json")
@@ -52,30 +54,26 @@ class Hud : Disposable {
   fun setup() {
     stage.clear()
 
-    val window = window("") {
-      isModal = false
-      isMovable = false
-      isResizable = false
+    val table = table {
+      debug = true
       height = Gdx.graphics.height.toFloat() / 6
       width = Gdx.graphics.width.toFloat()
-      horizontalFlowGroup {
-        inventoryTable = table (true){
-          setFillParent(true)
-          listView(inventoryListAdapter) {
-            header = label("Inventory")
-          }
+      cell
+      listView(inventoryListAdapter) {
+          header = label("Inventory")
         }
-      }
+      left()
+      top()
     }
-    stage.addActor(window)
+    stage.addActor(table)
     hideInventory()
   }
 
   fun showInventory() {
-    inventoryTable.isVisible = true
+  //  inventoryTable.isVisible = true
   }
 
   fun hideInventory() {
-    inventoryTable.isVisible = false
+//    inventoryListView.isVisible = false
   }
 }
