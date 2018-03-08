db51b566d2c9362afbe31e82a8b111d28d43f936
Tommie Nygren <tommie.nygren@gmail.com>
2018-03-08 18:34:33 +0100

    WIP - Monster Spawn
    For some reason I decided to implement my own BT.

--- kftw/src/core/src/com/lavaeater/kftw/ai/MonsterTasks.kt
+++ kftw/src/core/src/com/lavaeater/kftw/ai/MonsterTasks.kt
@@ -12,6 +12,19 @@ framework around to get stuff done.
 In this case, the npc should have some kind of food preference, a "type", sort of like
 will it eat anything or what?
  */
+
+/*
+
+All roaming creatures should be driven by needs. Animals that are not warmblooded
+can lay dormant for weeks (snakes) waiting to spring a trap on a victim. Others can walk extremely
+long distances to get food (wolves).
+
+Others must use their intelligence to gather food. Now, we don't need to make a complex framework
+for food or energy, but getting food / loot can be done in different ways and different animals
+should be able to employ different strategies, somehow.
+
+ */
+
 class SearchForFood : LeafTask<Npc>() {
   override fun execute(): Status {
     val npc = `object`
@@ -26,7 +39,7 @@ class SearchForFood : LeafTask<Npc>() {
       return Task.Status.SUCCEEDED
     }
 
-    return Task.Status.
+    return Task.Status.RUNNING
   }
 
   override fun copyTo(task: Task<Npc>?): Task<Npc> {
