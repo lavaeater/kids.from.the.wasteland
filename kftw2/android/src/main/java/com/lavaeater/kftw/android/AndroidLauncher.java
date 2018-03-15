eccf590c0a726f482f62c4238ef8932649c276d4
Tommie Nygren <tommie.nygren@vironova.com>
2018-03-15 14:20:20 +0100

    Created new  prject shell for android etc

---
+++ kftw2/android/src/main/java/com/lavaeater/kftw/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.lavaeater.kftw.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.lavaeater.kftw.KidsFromTheWastelandGame;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        initialize(new KidsFromTheWastelandGame(), configuration);
+    }
+}
---
+++ kftw2/lwjgl3/src/main/java/com/lavaeater/kftw/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,24 @@
+package com.lavaeater.kftw.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.lavaeater.kftw.KidsFromTheWastelandGame;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+    public static void main(String[] args) {
+        createApplication();
+    }
+
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new KidsFromTheWastelandGame(), getDefaultConfiguration());
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle("KidsFromTheWasteland");
+        configuration.setWindowedMode(640, 480);
+        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        return configuration;
+    }
+}
