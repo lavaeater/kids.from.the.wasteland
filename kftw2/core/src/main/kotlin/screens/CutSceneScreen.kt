5759546abb570f2f8a913f5f9921ef1871647705
Tommie Nygren <tommie.nygren@gmail.com>
2018-03-19 12:44:23 +0100

    Added cutscenescreen

---
+++ kftw2/core/src/main/kotlin/screens/CutSceneScreen.kt
@@ -0,0 +1,25 @@
+package screens
+
+import ktx.app.KtxScreen
+
+/**
+ * Created by tommie on 2018-03-18.
+ */
+class CutSceneScreen : KtxScreen {
+
+  override fun show() {
+    super.show()
+  }
+
+  override fun resize(width: Int, height: Int) {
+    super.resize(width, height)
+  }
+
+  override fun render(delta: Float) {
+    super.render(delta)
+  }
+
+  override fun hide() {
+    super.hide()
+  }
+}
