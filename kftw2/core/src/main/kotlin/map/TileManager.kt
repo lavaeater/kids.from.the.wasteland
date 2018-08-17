075f3e03ff6e94fb9c90bf2c307b4f61746664f1
Tommie Nygren <TommieN@vironova.com>
2018-08-17 14:41:38 +0200

    WIP - Locations
    Started the reimplementation of TileMap / TileManager to connect it
    to a location and make it serializable

--- kftw2/core/src/main/kotlin/map/TileManager.kt
+++ kftw2/core/src/main/kotlin/map/TileManager.kt
@@ -131,22 +131,184 @@ class TileMap(private val chunkSize: Int = 64,
               private val tileGenerator: TileGenerator) {
 
     private val upperBound = chunkSize - 1
+    private val tileStores = mutableSetOf<TileStoreBase>()
 
-    private val tileStore = mutableSetOf<TileStoreBase>()
-    private var currentAlgorithm : (xBounds: IntRange, yBounds: IntRange)-> Array<Array<TileInstance>>
-
-    init {
-        currentLocation = "worldmap"
-        currentTileStores = mutableSetOf()
-        locations[currentLocation] = currentTileStores
-        locationAlgorithms[currentLocation] = ::generateTilesForRange
-        currentAlgorithm = locationAlgorithms[currentLocation]!!
+    private fun lowerBoundFor(coordinate: Int): Int {
+        if (coordinate < 0) {
+            return ((coordinate + 1) / chunkSize) * chunkSize - chunkSize
+        }
+        return (coordinate / chunkSize) * chunkSize
     }
+
+    private fun getTileStore(x: Int, y: Int): ITileStore {
+        val lowerBoundX = lowerBoundFor(x)
+        val lowerBoundY = lowerBoundFor(y)
+        return getTileStoreLowerBounds(lowerBoundX, lowerBoundY)
+    }
+
+    private fun getTileStoreLowerBounds(lX: Int, lY: Int): ITileStore {
+        var store = tileStores.firstOrNull {
+            lX in it.xBounds &&
+                lY in it.yBounds
+        }
+        if (store == null) {
+            store = FlatTileStore(
+                lX,
+                chunkSize,
+                lY,
+                chunkSize,
+                tileGenerator.generateTilesForRange(
+                    lX..(lX + upperBound),
+                    lY..(lY + upperBound)))
+            tileStores.add(store)
+        }
+        return store
+    }
+
+    fun getTile(x: Int, y: Int): TileInstance {
+        val store = getTileStore(x, y)
+        return store.getTile(x, y)
+    }
+
+    fun getTilesFlat(xBounds: IntRange, yBounds: IntRange) : Array<TileInstance> {
+
+        lateinit var currentTile: TileInstance
+        var currentStore = getTileStore(xBounds.start, yBounds.start)
+        val columns = xBounds.count()
+        val rows = yBounds.count()
+        val size = columns * rows
+        var row = 0
+        var column = 0
+        return Array(size) { _ ->
+
+            val x = xBounds.start + column
+            val y = yBounds.start + row
+
+            if (x !in currentStore.xBounds || y !in currentStore.yBounds) {
+                currentStore = getTileStore(x, y)
+            }
+            currentTile = currentStore.getTile(x, y)
+
+            if (currentTile.tile.needsNeighbours) {
+                fixNeighbours(currentTile.tile, x, y).updateInstance(currentTile)
+//                putTile(x, y, currentTile)
+            }
+
+            column++
+            if(column > xBounds.count() - 1) {
+                column = 0
+                row++
+            }
+
+            return@Array currentTile
+        }
+    }
+
+    private fun getOrNull(x: Int, y: Int, tiles: Array<Array<Tile>>? = null): Tile? {
+        if (tiles == null) { //Use the manager to get the tile to check!
+            return getTile(x, y).tile
+        }
+
+        val col = tiles.getOrNull(x)
+        if (col != null) {
+            val tile = col.getOrNull(y)
+            if (tile != null) {
+                return tile
+            }
+        }
+        return null
+    }
+
+    private fun fixNeighbours(tile: Tile, x: Int, y: Int, tiles: Array<Array<Tile>>? = null): Tile {
+        val tempTile = tile.copy()
+        var needsNeighbours = false
+
+
+        MapService.neiborMap.keys.forEach { (offX, offY) ->
+            var code = "b"
+            val t = getOrNull(x + offX, y + offY, tiles)
+            if (t != null) {
+                code = MapService.shortTerrains[t.priority]!!
+            } else {
+                needsNeighbours = true
+            }
+            tempTile.code += code
+        }
+
+        tempTile.needsNeighbours = needsNeighbours
+        tempTile.shortCode = tempTile.code.toShortCode()
+
+        val keyCode = tempTile.getKeyCode()
+
+        if (!UsedTiles.tileExists(keyCode)) {
+            //Add this new tile to the tile storage!
+            addEdgeSpritesForTile(tempTile.shortCode, tempTile.priority)
+
+            UsedTiles.put(keyCode, tempTile)
+        }
+        return UsedTiles.get(keyCode)
+    }
+
+    private fun addEdgeSpritesForTile(shortCode: String, priority: Int) {
+
+        if (!MapService.noExtraSprites.contains(shortCode) && !Assets.codeToExtraTiles.containsKey(shortCode)) {
+            val tileC = shortCode.toCharArray()[0]
+            val actualShortCode = shortCode.substring(1..4) //leave out the tile itself.
+            val extraSprites = mutableListOf<Pair<String, String>>()
+            val extraSpritesToRemove = mutableListOf<Pair<String, String>>()
+
+            for ((index, code) in actualShortCode.withIndex()) {
+                if (code != 'b' && tileC != code && MapService.shortTerrainPriority[code]!! > priority) {
+                    if (extraSprites.any { it.first == MapService.shortLongTerrains[code]!! }) {
+                        //Evaluate for diffs etc
+                        for (extraSprite in extraSprites.filter { it.first == MapService.shortLongTerrains[code]!! }) {
+
+                            /*
+                      This type of tile exists, it might actually be relevant to
+                      remove the existing one in favor of this one!
+                       */
+                            if (MapService.weirdDirections.containsKey("${getDirectionFromIndex(index)}${extraSprite.second}")) {
+                                //Modify existing one, making it weird!
+                                extraSpritesToRemove.add(extraSprite)
+                                extraSprites.add(Pair(extraSprite.first, MapService.weirdDirections["${getDirectionFromIndex(index)}${extraSprite.second}"]!!))
+                            } else {
+                                extraSprites.add(Pair(MapService.shortLongTerrains[code]!!, getDirectionFromIndex(index)))
+                            }
+                        }
+                    } else {
+                        //just add
+                        extraSprites.add(Pair(MapService.shortLongTerrains[code]!!, getDirectionFromIndex(index)))
+                    }
+                }
+            }
+            for (extraSprite in extraSpritesToRemove) {
+                extraSprites.remove(extraSprite)
+            }
+
+            if (extraSprites.any())
+                Assets.codeToExtraTiles[shortCode] = extraSprites.map { Assets.tileSprites[it.first]!![it.second]!! }
+            else
+                MapService.noExtraSprites.add(shortCode)
+        }
+    }
+
+
 }
 
 class UsedTiles {
     companion object {
-        val usedTiles = mutableMapOf<String, Tile>()
+        private val tiles = mutableMapOf<String, Tile>()
+        fun tileExists(code: String) : Boolean {
+            return tiles.containsKey(code)
+        }
+
+        fun put(code: String, tile:Tile) {
+            tiles[code] = tile
+        }
+
+        fun get(code: String):Tile {
+            return tiles[code]!!
+        }
     }
 }
 
