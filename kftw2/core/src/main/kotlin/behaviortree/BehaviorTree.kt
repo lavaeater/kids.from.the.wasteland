aa8d9cec4f60e8249c8d4088c16e68ef4ad13085
Tommie Nygren <TommieN@vironova.com>
2018-09-05 16:27:09 +0200

    WIP - BT implementation!

--- kftw2/core/src/main/kotlin/behaviortree/BehaviorTree.kt
+++ kftw2/core/src/main/kotlin/behaviortree/BehaviorTree.kt
@@ -7,10 +7,14 @@ enum class NodeStatus {
 }
 
 interface INode {
+  fun init()
   fun run(): NodeStatus
 }
 
-abstract class Node<T>(val blackBoard:T) : INode
+abstract class Node<T>(val blackBoard:T) : INode {
+  override fun init() {
+  }
+}
 
 abstract class LeafNode<T>(blackBoard: T):Node<T>(blackBoard)
 
@@ -19,16 +23,18 @@ abstract class CompositeNode<T>(blackBoard: T, val children: List<INode>) : Node
 abstract class DecoratorNode<T>(blackBoard: T, val child: INode):Node<T>(blackBoard)
 
 class Sequence<T>(blackBoard: T, children: List<INode>) : CompositeNode<T>(blackBoard, children) {
-
   var currentIndex = 0;
 
   override fun run(): NodeStatus {
-//    while ()
+    if( currentIndex in 0 until children.size) {
+      currentIndex++ //increment AFTER execution!
+    }
     return NodeStatus.RUNNING
   }
+}
 
-//  fun executeNextChild() : NodeStatus {
-//
-//  }
-
+class Selector<T>(blackBoard: T, children: List<INode>) : CompositeNode<T>(blackBoard, children) {
+  override fun run(): NodeStatus {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
 }
---
+++ kftw2/core/src/test/kotlin/BehaviorTests.kt
@@ -0,0 +1,27 @@
+
+import org.junit.After
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+
+class BehaviorTests {
+
+	companion object {
+		@JvmStatic
+		@BeforeClass
+		fun beforeClass() {
+		}
+	}
+
+	@Before
+	fun before() {
+	}
+
+	@After
+	fun after() {
+	}
+
+	@Test
+	fun addStringToList_ListContainsString() {
+	}
+}
