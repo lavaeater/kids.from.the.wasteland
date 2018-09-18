3f71d24af4647f05f4774c66b4d685f4192f2463
Tommie Nygren <TommieN@vironova.com>
2018-09-18 15:25:20 +0200

    WIP - Graph
    Added some interfaces and some base classes and stuff

--- kftw2/core/src/test/kotlin/GraphTests.kt
+++ kftw2/core/src/test/kotlin/GraphTests.kt
@@ -19,3 +19,35 @@ class GraphTests {
 	fun after() {
 	}
 }
+
+interface IGraph {
+
+}
+
+interface INode {
+	val id: Int
+	val nodes
+}
+
+interface IRelation {
+	val id: Int
+}
+
+interface ILabel {
+	val value: String
+}
+
+interface IProperty<T> {
+	val key: String
+	var value: T
+}
+
+interface INumberProperty<T: Number> : IProperty<T>
+
+abstract class Property<T> : IProperty<T>
+
+abstract class NumberProperty<T: Number> : INumberProperty<T>
+
+data class StringProperty(override val key: String, override var value: String) : Property<String>()
+data class BooleanProperty(override val key: String, override var value: Boolean): Property<Boolean>()
+data class IntegerProperty(override val key: String, override var value: Integer): NumberProperty<Integer>()
