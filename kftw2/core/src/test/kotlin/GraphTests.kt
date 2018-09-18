91388dfff62ebc3a227c418b9781d837da5e4633
Tommie Nygren <TommieN@vironova.com>
2018-09-18 14:30:06 +0200

    WIP Graph DB

---
+++ kftw2/core/src/test/kotlin/GraphTests.kt
@@ -0,0 +1,21 @@
+import org.junit.After
+import org.junit.Before
+import org.junit.BeforeClass
+
+class GraphTests {
+
+	companion object {
+		@JvmStatic
+		@BeforeClass
+		fun beforeClass() {
+		}
+	}
+
+	@Before
+	fun before() {
+	}
+
+	@After
+	fun after() {
+	}
+}
