22decbf22cd3ead0de0df1a626dad9e86e4cc767
Tommie Nygren <tommie.nygren@gmail.com>
2018-09-20 22:43:51 +0200

    WIP Graphs - some good stuff, some bad
    Finding min/max is slow as fudge

--- kftw2/core/src/main/kotlin/graph/Graph.kt
+++ kftw2/core/src/main/kotlin/graph/Graph.kt
@@ -1,9 +1,40 @@
 package graph
 
-class Graph {
+class Graph(val graphProperties: Map<String, Any>) {
 	val nodes = mutableSetOf<Node>()
 	private val labels = mutableMapOf<String, MutableSet<Node>>()
-	private val properties = mutableMapOf<String, Pair<Node, PropertyValue>>()
+
+	/*
+	Different concept could be having the properties as a map of
+	properties and then their values and THEN nodes that have those values...
+	 */
+	private val proppo = mutableMapOf<String, MutableMap<Property, MutableList<Node>>>()
+	private val properties = mutableMapOf<String, Pair<Node, Property>>()
+
+	fun addProperty(node: Node, property: Property) {
+		if(!proppo.containsKey(property.name))
+			proppo[property.name] = mutableMapOf()
+
+		if(!proppo[property.name]!!.containsKey(property))
+			proppo[property.name]!![property] = mutableListOf()
+
+		proppo[property.name]!![property]!!.add(node)
+	}
+
+
+
+	fun addLabel(label:String, node:Node) {
+		if(!labels.containsKey(label))
+			labels[label] = mutableSetOf()
+
+		labels[label]!!.add(node)
+	}
+
+	fun removeLabel(label:String, node:Node) {
+		if(!labels.containsKey(label)) return
+
+		labels[label]!!.remove(node)
+	}
 
 	fun addNode(node: Node) {
 		nodes.add(node)
@@ -40,16 +71,16 @@ open class Node {
 
 data class TypedNode<T>(val data: T) : Node()
 
-abstract class PropertyValue
-
-abstract class TypedPropertyValue<T> {
-	abstract var value: T
+abstract class Property {
+	abstract val name: String
 }
 
-data class GenericPropertyValue<T>(override var value:T):TypedPropertyValue<T>()
+abstract class TypedProperty<T> : Property() {
+	abstract val value: T
+}
 
-data class StringValue(override var value: String) : TypedPropertyValue<String>()
-data class IntValue(override var value: Int) : TypedPropertyValue<Int>()
-data class BoolValue(override var value: Boolean) : TypedPropertyValue<Boolean>()
-
-data class Coordinate(val x: Int, val y: Int)
+data class GenericTypedProperty<T>(override val name: String, override val value:T):TypedProperty<T>()
+data class StringProperty(override val name: String, override val value: String) : TypedProperty<String>()
+data class IntProperty(override val name: String, override val value: Int) : TypedProperty<Int>()
+data class BoolProperty(override val name: String, override val value: Boolean) : TypedProperty<Boolean>()
+data class Coordinate(val x: Int, val y: Int, var type: Int = 0)
--- kftw2/core/src/test/kotlin/GraphTests.kt
+++ kftw2/core/src/test/kotlin/GraphTests.kt
@@ -1,6 +1,7 @@
 
 import graph.Coordinate
 import graph.Graph
+import graph.Node
 import graph.TypedNode
 import org.junit.BeforeClass
 import kotlin.test.Test
@@ -16,17 +17,15 @@ class GraphTests {
 	}
 
 	@Test
-	fun addAndFindRelations() {
-		val graph = createGrid()
+	fun graphAsMap() {
+		val graph = createGrid(20, 20)
 
-		assertEquals(64, graph.nodes.count())
-
-		val test = ""
-
+		assertEquals(400, graph.nodes.count())
+		println(graph.nodes.map { it as TypedNode<Coordinate> }.prettyPrint())
 	}
 
 	fun createGrid(width: Int = 8, height: Int = 8) : Graph {
-		val g = Graph()
+		val g = Graph(mapOf("width" to width, "height" to height))
 
 		val nodes = mutableListOf<TypedNode<Coordinate>>()
 
@@ -66,4 +65,35 @@ class GraphTests {
 		}
 		return direction
 	}
+}
+
+fun Collection<TypedNode<Coordinate>>.prettyPrint() :String {
+	//1. find top left coordinate
+	/*
+	We know the dimensions
+
+	Another way is to traverse the node relations. So we start
+	with the "top left" node, then just go east until we come to a node does
+	not have a neighbour to the east, then just down from the first one!
+	 */
+	val sb = StringBuilder()
+	val minX = this.map { it.data.x }.min()!!
+	val maxY = this.map { it.data.y }.max()!!
+	var topLeft: TypedNode<Coordinate>? = this.first { it.data.x == minX && it.data.y == maxY }
+
+	var currentNode : TypedNode<Coordinate>? = topLeft
+
+	while (currentNode != null) {
+		sb.append(currentNode.data.type)
+		currentNode = currentNode.neigbours("east").firstOrNull() as TypedNode<Coordinate>?
+		if(currentNode == null) {
+			currentNode = topLeft?.neigbours("south")?.firstOrNull() as TypedNode<Coordinate>?
+			topLeft = currentNode
+			sb.appendln()
+		}
+	}
+
+	return sb.toString()
+
+	//2. What do we know of the height and width of the graph?
 }
