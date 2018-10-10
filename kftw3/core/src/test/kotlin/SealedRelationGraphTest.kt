b340a954f66ab022f51f33159c168027257e4939
Tommie Nygren <tommie.nygren@gmail.com>
2018-10-10 20:16:50 +0200

    WWUUAT? WIP - Map as graph and puzzle

--- kftw3/core/src/test/kotlin/SealedRelationGraphTest.kt
+++ kftw3/core/src/test/kotlin/SealedRelationGraphTest.kt
@@ -39,27 +39,7 @@ sealed class MapRelations {
 
 class SealedRelationGraphTest {
   companion object {
-    val dirs: Map<CompassDirection, Pair<Int, Int>>
-      get() = mapOf(
-          CompassDirection.NORTH      to Pair(0,1),
-          CompassDirection.NORTHEAST  to Pair(1,1),
-          CompassDirection.EAST       to Pair(1,0),
-          CompassDirection.SOUTHEAST  to Pair(1,-1),
-          CompassDirection.SOUTH      to Pair(0,-1),
-          CompassDirection.SOUTHWEST  to Pair(-1,-1),
-          CompassDirection.WEST       to Pair(-1,0),
-          CompassDirection.NORTHWEST  to Pair(-1,1))
 
-    val opposites: Map<CompassDirection, CompassDirection>
-      get() = mapOf(
-          CompassDirection.NORTH      	to 	CompassDirection.SOUTH,
-          CompassDirection.NORTHEAST   to 	CompassDirection.SOUTHWEST,
-          CompassDirection.EAST       	to 	CompassDirection.WEST,
-          CompassDirection.SOUTHEAST   to 	CompassDirection.NORTHWEST,
-          CompassDirection.SOUTH      	to 	CompassDirection.NORTH,
-          CompassDirection.SOUTHWEST   to 	CompassDirection.NORTHEAST,
-          CompassDirection.WEST       	to 	CompassDirection.EAST,
-          CompassDirection.NORTHWEST   to 	CompassDirection.SOUTHEAST)
 
     @JvmStatic
     @BeforeClass
@@ -69,6 +49,64 @@ class SealedRelationGraphTest {
   }
 
   @Test
-  fun basicTypedTest() {
+  fun worldMap_Creation() {
+
+
+  }
+}
+
+object MapStuff {
+  val dirs: Map<CompassDirection, Pair<Int, Int>>
+    get() = mapOf(
+        CompassDirection.NORTH      to Pair(0,1),
+        CompassDirection.NORTHEAST  to Pair(1,1),
+        CompassDirection.EAST       to Pair(1,0),
+        CompassDirection.SOUTHEAST  to Pair(1,-1),
+        CompassDirection.SOUTH      to Pair(0,-1),
+        CompassDirection.SOUTHWEST  to Pair(-1,-1),
+        CompassDirection.WEST       to Pair(-1,0),
+        CompassDirection.NORTHWEST  to Pair(-1,1))
+
+  val opposites: Map<CompassDirection, CompassDirection>
+    get() = mapOf(
+        CompassDirection.NORTH      	to 	CompassDirection.SOUTH,
+        CompassDirection.NORTHEAST   to 	CompassDirection.SOUTHWEST,
+        CompassDirection.EAST       	to 	CompassDirection.WEST,
+        CompassDirection.SOUTHEAST   to 	CompassDirection.NORTHWEST,
+        CompassDirection.SOUTH      	to 	CompassDirection.NORTH,
+        CompassDirection.SOUTHWEST   to 	CompassDirection.NORTHEAST,
+        CompassDirection.WEST       	to 	CompassDirection.EAST,
+        CompassDirection.NORTHWEST   to 	CompassDirection.SOUTHEAST)
+}
+
+object MapBuilder {
+
+
+
+  fun getNodes(width: Int, height: Int) : Array<Array<Node<Coordinate, String>>> {
+    return Array<Array<Node<Coordinate, String>>>(width) { x ->
+      Array<Node<Coordinate, String>>(height) { y ->
+        Node(Coordinate(x, y))
+      }
+    }
+  }
+
+  fun fixNeighbours(nodes: Array<Array<Node<Coordinate, MapRelations>>>) {
+    val maxX = nodes.lastIndex
+    val maxY = maxX //Symmetric
+    for((x, rows) in nodes.withIndex())
+      for((y, node) in rows.withIndex()) {
+        for((direction, offset) in MapStuff.dirs) {
+          if(!node.hasRelation(direction)) {
+            val tX = x + offset.first
+            val tY = y + offset.second
+            if(tX in 0..maxX && tY in 0..maxY) {
+              val tNode = nodes[tX]!![tY]!!
+              node.addRelation(direction, tNode)
+              tNode.addRelation(dirs2[direction]!!, node)
+            }
+          }
+        }
+      }
   }
 }
