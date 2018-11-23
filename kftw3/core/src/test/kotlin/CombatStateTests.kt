e06064ba19d8199bdc95f53aff7654b835b2a90d
Tommie Nygren <TommieN@vironova.com>
2018-11-23 14:49:05 +0100

    WIP CRS
    Worked some more on the concept of discipline vs all other things
    the idea of victory points must not be discontinued

--- kftw3/core/src/test/kotlin/CombatStateTests.kt
+++ kftw3/core/src/test/kotlin/CombatStateTests.kt
@@ -1,147 +1,43 @@
 import kotlin.test.Test
 
 /*
-The state of Combat, USA.
-
-Combat is a state machine competition.
-
-Every round both teams try to affect their own and the others
-state. Everything you can do in combat affects your ability to
-change your own and your opponents' state.
-
-The Basic States we have are (we might add more later)
-Overwhelmed - the team has been ambushed, this is a start state
-Pushed Back - the team loses ground and advantage, morale is deteriorating quickly
-Pinned - the team is controlled by the opponents, leaving them little room to act
-Disciplined - the team is in control of their situation and can make good decisions
-Dominating - the team controls / dominates the opponent, gains ground, gets advantages
-Defeated - the team has lost - end state
-Victorious - the team has won - end state
-Disengaged - the team has managed to leave the encounter
-
-Add states for bargaining.
-
-Moving between states is done by making moves. Different moves
-require different skills and stats and are not always available in all modes.
-
-A controlled team might not be able to do a tactical retreat (escape routes are cut off) or it might
-at least be very hard. A team can always make a complete break for it, but that is dangerous.
-
-Every action in combat yields a result. This result dictates what state one ends up
-in next. The result is sort of an "event", used by the state machine.
-
-Possible Events?
-
-UltraLoss
-MajorLoss
-Loss
-Neutral
-Win
-MajorWin
-UltraWin
-
-Perhaps the moves and their definitions should be defining the state machine?
-What if states and events were strings instead of enums?
-
-Could we make a list of moves, states and events that could work with that?
-
-
+Combat IS a state machine, but that state machine OOONLY keeps
+track of the combat loop, for simplicity
  */
 
 enum class SkillOutcome {
   CriticalFailure, //Natural 1
+  MajorFailure,
   Failure, //Not over skill
   Success, //Over skill
   MajorSuccess, //High bonus value
   UltraSuccess //Natural 20 or more, high bonus
 }
 
-/**
- * The combatmove class is a definition of a combat move
- * with its modifiers, requirements, bonuses and of course,
- * states in which it can be performed. Some combatmoves
- * can be performed in a special context, i.e. before a combat
- * one can perform an ambush, for instance.
- */
+class CombatAgent(
+    val name: String,
+    var discipline: Int = DisciplineLevels.levelOf(DisciplineLevels.Neutral))
 
-/*
-Jesus. Is the combat shit a fucking graph?
+object DisciplineLevels {
 
-Is it a fucking graph I ask you?
-
-If it were, the combatagent could have a current state (a node)
-with a list of possible moves (its relations). If successful,
-every move has a list of outcomes, that are in turn fucking nodes.
- */
-//
-//object GraphAss {
-//  val allNodes = mutableMapOf<String, Node<String, CombatRelation>>()
-//  val g = Graph<String, CombatRelation>(emptyMap())
-//  init {
-//    for (state in CombatStates.AllStates) {
-//      allNodes[state] = g.addNode(CombatNode.CombatState(state))
-//    }
-//  }
-//}
-//
-//sealed class CombatNode(open val name: String)  : Node<String, CombatRelation>(name) {
-//  class CombatMove(override val name:String) : CombatNode(name)
-//  class CombatState(override val name:String) : CombatNode(name)
-//}
-//
-//@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator::class, property = "id")
-//sealed class CombatRelation {
-//  var id = UUID.randomUUID()
-//  data class CombatMoveRelation(val name:String) : CombatRelation()
-//  data class StateRelation(val outcome: SkillOutcome) : CombatRelation()
-//}
-
-class CombatAgent(val name: String, var state: String)
-
-/**
- * The event disengaged can be conjured up from some
- * other state and it would obviously put that team
- * in the state Disengaged. So we map outcomes -> events
- * and these events map to states, of course.
- *
- * So the state machine is built by saying that all states can be
- * gotten to from all other states by a certain event (the state)
- *
- * For that... we don't need a state machine.
- *
- * Or actually, the mapping is now outcomes -> states
- * But we need a probability mapping for a move -> what states
- * it can lead to.
- *
- * I think, that for now, we could skip the state machine altother and just
- * consider this an excercise in mapping moves / skills -> outcomes -> states.
- *
- * Like if Team A performs some move, it can effect their opponents state AND
- * their own
- *
- */
-
-object CombatStates {
-  const val Ambushed = "Ambushed"
-  const val Overwhelmed = "Overwhelmed" //Not a good look
-  const val Disengaged = "Disengaged" //Combat ends
-  const val Disciplined = "Disciplined" //Team is doing well
   const val Dominating = "Dominating" //Doing lots of damage and disciplined
-  const val Defeated = "Defeated" //Combat ends
-  const val Pinned = "Pinned" //Limited options
+  const val Disciplined = "Disciplined" //Team is doing well
   const val Neutral = "Neutral" //Neutral
-  const val Victorious = "Victorious"  //combat ends
-  const val NeutralOrHigher = "NeutralOrHigher"
+  const val Pinned = "Pinned" //Limited options
+  const val Overwhelmed = "Overwhelmed" //Not a good look
+  const val Panic = "Panic"
 
-  val AllStates = setOf(
-      Ambushed,
+  fun levelOf(level:String):Int {
+    return Levels.indexOf(level)
+  }
+
+  val Levels = sortedSetOf(
+      Panic,
       Overwhelmed,
-      Disengaged,
-      Disciplined,
-      Defeated,
       Pinned,
       Neutral,
-      Victorious)
+      Disciplined,
+      Dominating)
 }
 
 /*
@@ -161,25 +57,27 @@ object SomeMaps {
       10..14 to SkillOutcome.MajorSuccess,
       1..9 to SkillOutcome.Success,
       -9..0 to SkillOutcome.Failure,
-      -200..-10 to SkillOutcome.CriticalFailure)
+      -19..10 to SkillOutcome.MajorFailure,
+      -200..-20 to SkillOutcome.CriticalFailure)
 
   val regularModifier = mapOf(
       SkillOutcome.UltraSuccess to 10,
       SkillOutcome.MajorSuccess to 5,
       SkillOutcome.Success to 2,
-      SkillOutcome.Failure to -5,
+      SkillOutcome.Failure to -3,
+      SkillOutcome.MajorFailure to -5,
       SkillOutcome.CriticalFailure to -10
   )
 
   val regularSuccessStates = mapOf(
-      SkillOutcome.Success to CombatStates.Neutral,
-      SkillOutcome.MajorSuccess to CombatStates.Disciplined,
-      SkillOutcome.UltraSuccess to CombatStates.Dominating
+      SkillOutcome.Success to DisciplineLevels.Neutral,
+      SkillOutcome.MajorSuccess to DisciplineLevels.Disciplined,
+      SkillOutcome.UltraSuccess to DisciplineLevels.Dominating
   )
 
   val regularFailStates = mapOf(
-      SkillOutcome.Failure to CombatStates.Pinned,
-      SkillOutcome.CriticalFailure to CombatStates.Overwhelmed)
+      SkillOutcome.Failure to DisciplineLevels.Pinned,
+      SkillOutcome.CriticalFailure to DisciplineLevels.Overwhelmed)
 }
 
 object SkillDifficulty {
@@ -210,7 +108,7 @@ Neutral
 Pinned
 Overwhelmed - same as ambushed then, your opponent succeeds in ambushing you, you start at
 a moral disadvantage
-Routed - panicked
+Panic
 
 This makes it easy to map going up and down in levels of morale!
 
@@ -226,17 +124,44 @@ things:
 Defeat - Negotiation / Neutral?
 Victory - Negotiation / Neutral?
 
-This might work. Getting there, slooowly
+Victory
 
+Victory - Disengaged (for the team that succeeds in a disengage move
+Defeat - Disengaged (team that was "left behind")
 
+Victory - Retreat
+Defeat - Retreat
+
+Victory is obviously defined as the team SUCCEEDING with something.
+
+We should perhaps call that
+POSITIVE
+and non-wanted outcomes as NEGATIVE.
+
+Meaning that for instance team A performs the move Pin on team B. Team B on the other hand
+tries to Disengage. Pin *can* lead to Victory but in this case Team A simply succeed with their
+skill roll
+
+But when, when does one actually "win"?
+
+Perhaps we can work with...
+
+We need to pick ONE definition of victory that can work for *now*. That definition can
+change later.
+
+So - being victorious only means you got what you wanted. Losing means not being able to
+control what happens in the combat at all, anymore.
+
+So, failing the discipline roll at the end of the round *can* result in defeat. The nature of that
+defeat is contingent on the action of the opponent. Say that they tried to Pin their opponents.
+They actually failed this time but the opposing team is already in bad shape and gets a
+serious disadvantage on their discipline roll. The get a failure and lose the fight.
 
  */
 
 data class CombatMove(
     val name: String,
-    val validStates: Set<String> = CombatStates.AllStates,
-    val successStates: Map<SkillOutcome, String> = SomeMaps.regularSuccessStates,
-    val failStates: Map<SkillOutcome, String> = SomeMaps.regularFailStates,
+    val minDisciplineLevel: Int = DisciplineLevels.levelOf(DisciplineLevels.Neutral),
     val difficulty: Int = SkillDifficulty.Medium,
     val damageModifier: Map<SkillOutcome, Int> = SomeMaps.regularModifier,
     val defensiveModifier: Map<SkillOutcome, Int> = SomeMaps.regularModifier,
@@ -245,21 +170,7 @@ data class CombatMove(
 object CombatMoves {
   val moves = setOf(
       CombatMove("Shell",
-          setOf(
-              CombatStates.Neutral,
-              CombatStates.Overwhelmed,
-              CombatStates.Disciplined,
-              CombatStates.Pinned,
-              CombatStates.Dominating),
-          mapOf(
-              SkillOutcome.Success to CombatStates.Neutral, //Needs work, I guess
-              SkillOutcome.MajorSuccess to CombatStates.Disciplined,
-              SkillOutcome.UltraSuccess to CombatStates.Dominating
-          ),
-          mapOf(
-              SkillOutcome.Failure to CombatStates.NeutralOrHigher,
-              SkillOutcome.CriticalFailure to CombatStates.Pinned
-          )),
+          DisciplineLevels.levelOf(DisciplineLevels.Pinned)),
       CombatMove("Fire at will"),
       CombatMove("Controlled fire"),
       CombatMove("Negotiate"),
@@ -267,19 +178,7 @@ object CombatMoves {
       CombatMove("Flank"),
       CombatMove("Disengage"),
       CombatMove("Pin",
-          setOf(CombatStates.Disciplined),
-          mapOf(
-              SkillOutcome.Success to CombatStates.Disciplined,
-              SkillOutcome.MajorSuccess to CombatStates.Dominating,
-              SkillOutcome.UltraSuccess to CombatStates.Victorious),
-          mapOf(
-              SkillOutcome.Failure to CombatStates.Pinned,
-              SkillOutcome.CriticalFailure to CombatStates.Overwhelmed),
-          SkillDifficulty.Hard,
-          SomeMaps.regularModifier,
-          SomeMaps.regularModifier,
-          SomeMaps.regularModifier
-      )
+          DisciplineLevels.levelOf(DisciplineLevels.Disciplined))
   )
 }
 
